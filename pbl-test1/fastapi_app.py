6e68efe36f2254f510428404e2f7c51881d773ec
Moksh008 <mokshkulshrestha@gmail.com>
2026-05-19 20:33:07 +0530

    Docker File + CROS origin updated

--- pbl-test1/fastapi_app.py
+++ pbl-test1/fastapi_app.py
@@ -65,7 +65,7 @@ def get_detectors():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "https://smart-hire-8ysf.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
